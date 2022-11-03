e62e51d652120ee3ddf6bbd8f7e006486a944b87
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2022-11-03 07:46:58 +0100

    Fix request in invalid.py

--- examples/invalid.py
+++ examples/invalid.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath("./grewpy"))  # Use local grew lib
 import grew
 from grew import Corpus, Request
 
-r = Request("X<Y")
+r = Request("X[]; Y[]; X < Y")
 c = Corpus("examples/resources/fr_pud-ud-test.conllu")
 
 print(c.count(r))
