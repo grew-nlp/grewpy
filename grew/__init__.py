dc772c9e8a662874c382156e027537c6d6226342
Guillaume Bonfante <bonfante@loria.fr>
2022-11-11 11:16:41 +0100

    oups, needs init

---
+++ grew/__init__.py
@@ -0,0 +1,9 @@
+from .corpus import Corpus
+from .grs import Request, GRS, Package, Rule, Command
+from .graph import Graph
+from .network import init
+from . import utils
+
+from .grew import set_config
+
+init(False)
