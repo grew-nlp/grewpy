976bce081f1939fc9a1f0fab49cfcfc1907f911d
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2023-05-29 18:36:49 +0200

    rm duplicate code

--- grewpy/grs.py
+++ grewpy/grs.py
@@ -72,10 +72,6 @@ class Request():
     def without(self, *L):
         self.items += tuple(RequestItem("without", e) for e in L)
         return self
-    def with_(self, *L):
-        self.items += tuple(RequestItem("with", e) for e in L)
-        return self
-
 
     def with_(self, *L):
         self.items += tuple(RequestItem("with", e) for e in L)
