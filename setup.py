e30b91fbfb2d984868963ad04b1629926b8520cf
Bruno Guillaume <Bruno.Guillaume@loria.fr>
2024-02-21 10:34:03 +0100

    version 0.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='grewpy',
-    version='0.4.4',
+    version='0.4.5',
     packages=['grewpy','examples'],
     install_requires=[ 'numpy' ],
     license='LICENSE/Licence_CeCILL_V2-en.txt',
